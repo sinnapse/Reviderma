3c1f6c9f97c6fc25b09800bea8789135455fd6de
Luís <degaus@live.com>
2018-10-08 14:03:23 -0300

    Modifications on Gulp File

--- theme-child/gulpfile.js
+++ theme-child/gulpfile.js
@@ -1,41 +1,55 @@
-var gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    concat          = require('gulp-concat'),
-    uglify          = require('gulp-uglify'),
-    babel           = require('gulp-babel'),
-    minCSS          = require('gulp-clean-css'),
-    autoPrefixer    = require('gulp-autoprefixer');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  notify = require("gulp-notify"),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  babel = require("gulp-babel"),
+  minCSS = require("gulp-clean-css"),
+  autoPrefixer = require("gulp-autoprefixer");
 
-// Configuration Default
-gulp.task('default',['sass', 'js', 'watch']);
+// Configuration SASS with Notification Error
+gulp.task("sass", () => {
+  return gulp
+    .src("assets/src/sass/**/*.scss")
+    .pipe(concat("style.min.css")) // Concat
+    .pipe(
+      sass({ outputStyle: "compressed" }).on(
+        "error",
+        notify.onError({
+          title: "Tem um erro no style.scss",
+          message: "<%= error.message %>"
+        })
+      )
+    )
 
-// Configuration SASS
-gulp.task('sass', () => {
-    return gulp.src('assets/src/sass/**/*.scss')
-        .pipe(concat('style.min.css')) // Concat
-        .pipe(sass({outputStyle: 'compressed'})
-            .on('error', sass.logError))
-
-        .pipe(autoPrefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(minCSS())
-        .pipe(gulp.dest('assets/css'));
+    .pipe(
+      autoPrefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(minCSS())
+    .pipe(gulp.dest("assets/css"));
 });
 
-// Configuration Javascript
-gulp.task('js', () => {
-    return gulp.src('assets/src/js/**/*.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify())
-        .pipe(concat('script.min.js')) // Concat
-        .pipe(gulp.dest('assets/js'));
+// Configuration Javascript and Concat all types JS
+gulp.task("js", () => {
+  return gulp
+    .src("assets/src/js/**/*.js")
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(concat("script.min.js")) // Concat
+    .pipe(gulp.dest("assets/js"));
 });
 
 // Configuration watch for auto-run
-gulp.task('watch', () => {
-    gulp.watch('assets/src/sass/**/*.scss', ['sass']);
-    gulp.watch('assets/src/js/**/*.js', ['js']);
+gulp.task("watch", () => {
+  gulp.watch("assets/src/sass/**/*.scss", ["sass"]);
+  gulp.watch("assets/src/js/**/*.js", ["js"]);
 });
+
+// Configuration Default
+gulp.task("default", ["sass", "js", "watch"]);
